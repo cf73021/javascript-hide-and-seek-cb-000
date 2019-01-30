5fbd6a37a1f8beff1bc19a67bfaaaa937b42a569
Chris Franklin <cf73021@gmail.com>
2019-01-30 18:17:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector) {
+  selected = document.querySelector(selector)
+  return selected
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  ranks = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < ranks.length; i++){
+    ranks[i].innerHTML = (i + 1).toString();
+  }
+}
+
+function deepestChild(){
+  deepest = document.querySelector('#grand-node')
+  for (let i = 0; i < deepest.length; i++){
+    next_deepest = document.querySelectorAll()
+  }
+}
